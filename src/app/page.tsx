7353040899fa2a329ab98afd7c160237a9013072
emgarf <blozz@hotmail.com>
2023-07-02 18:10:25 +0200

    refacto: Change the logic for the data modifications in the page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,41 +9,50 @@ import Header from './components/header';
 
 export default function Home() {
 	const { data, loading, error } = useFetch('https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k');
-	const [searchItems, setSearchItems] = useState<Job[] | []>([]);
-	const [selectValues, setSelectValues] = useState<string>('department');
-
-	useEffect(() => {
-		if (data.length > 0) {
-			setSearchItems(data);
-		}
-	}, [data]);
+	const [search, setSearch] = useState<string>('');
+	const [results, setResults] = useState<Job[] | []>([]);
+	const [groupBy, setGroupBy] = useState<string>('department');
+	const [filterBy, setFilterBy] = useState<string[] | undefined>([]);
 
 	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const eventValue = event.target.value;
-		if (eventValue === undefined) {
-			setSearchItems(data);
-		} else {
-			setSearchItems(data?.filter((item) => item.name.toLowerCase().includes(eventValue.toLowerCase())));
-		}
+		setSearch(event.target.value);
 	};
 
 	const handleSelectChange = (value: string) => {
-		setSelectValues(value);
+		setGroupBy(value);
 	};
 
-	const filterResultsByCategory = () => {
-		if (selectValues !== 'none') {
-			return sortById(searchItems, selectValues);
-		} else {
-			return searchItems;
+	const handleFilterChange = (value: string[]) => {
+		setFilterBy(value);
+	};
+
+	// filter the jobs based on the search query, the "group by" field and the "filter by" field
+	useEffect(() => {
+		if (loading === true) return;
+		// res contains an array of all the jobs that match the search
+		const res = data.filter((item) => {
+			const name = item.name.toLowerCase();
+			return name.includes(search.toLowerCase());
+		});
+
+		const sortedResults = sortById(res, groupBy);
+		setResults(sortedResults);
+
+		// filter the sorted results
+		if (filterBy && filterBy?.length > 0) {
+			setResults(sortedResults.filter((item) => filterBy?.includes(item.contract_type.en)));
 		}
-	};
-
-	const results = filterResultsByCategory();
+	}, [data, search, loading, groupBy, filterBy, results]);
 
 	return (
 		<>
-			<Header selectValues={selectValues} handleChange={handleInputChange} handleSelectChange={handleSelectChange} />
+			<Header
+				selectGroupValues={groupBy}
+				selectFiltersValues={filterBy}
+				handleFilterChange={handleFilterChange}
+				handleChange={handleInputChange}
+				handleSelectChange={handleSelectChange}
+			/>
 
 			<Box p="xl" w={{ _: '100%', lg: '960px' }} m="auto">
 				<OfferList loading={loading} error={error} data={results} />
